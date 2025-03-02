8d0bb285d8795fe8fd212c2e076c394cc81d41de
Kumar_Anshuman <kanshuman6484@gmail.com>
2025-03-02 23:11:38 +0530

    book-hive

--- 05BookHive/src/App.jsx
+++ 05BookHive/src/App.jsx
@@ -1,24 +1,63 @@
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 import './App.css'
 
 export default function App() {
+  const [books, setBooks] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState(null)
+  const [query, setQuery] = useState('')
+
+  const API_KEY = 'AIzaSyDL_XKXHcVKn3RSOS7ayt0dprlAsw0-nAc'
+
+  useEffect(
+    function () {
+      async function fetchBooks() {
+        try {
+          setIsLoading(true)
+          setError(null)
+          const res = await fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`
+          )
+          if (!res.ok) throw new Error('Something went wrong')
+          const data = await res.json()
+          setBooks(data.items)
+        } catch (err) {
+          setError(err.message)
+        } finally {
+          setIsLoading(false)
+        }
+      }
+      if (!query) {
+        setBooks([])
+        setError(null)
+        return
+      }
+      fetchBooks()
+    },
+    [error, query]
+  )
+
   return (
-    <div className="flex flex-col h-screen bg-emerald-50">
-      <Header />
+    <div className="flex flex-col w-screen min-h-screen bg-emerald-50">
+      <Header query={query} setQuery={setQuery} />
       <main className="flex flex-1 container mx-auto p-6 space-x-6">
-        <LeftSection />
+        <LeftSection books={books} isLoading={isLoading} error={error} />
         <RightSection />
       </main>
     </div>
   )
 }
 
-function Header() {
+function Header({ query, setQuery }) {
   return (
     <header className="bg-emerald-500 p-4 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold text-white">BOOK HIVE</h1>
         <div className="flex items-center space-x-4">
           <input
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
             type="text"
             placeholder="Search books..."
             className="px-4 py-2 rounded-lg border border-emerald-300 focus:outline-none focus:ring-2 focus:ring-emerald-600"
@@ -30,28 +69,75 @@ function Header() {
   )
 }
 
-function LeftSection() {
+Header.propTypes = {
+  query: PropTypes.string,
+  setQuery: PropTypes.func,
+}
+
+function LeftSection({ books, isLoading, error }) {
   return (
     <section className="w-1/2 bg-white p-6 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold text-emerald-800 mb-4">
-        Search Results
-      </h2>
-      <ul className="space-y-2">
-        <li className="p-3 bg-emerald-100 rounded-lg hover:bg-emerald-200 cursor-pointer">
-          Book 1
-        </li>
-        <li className="p-3 bg-emerald-100 rounded-lg hover:bg-emerald-200 cursor-pointer">
-          Book 2
-        </li>
-        <li className="p-3 bg-emerald-100 rounded-lg hover:bg-emerald-200 cursor-pointer">
-          Book 3
-        </li>
-        <li className="p-3 bg-emerald-100 rounded-lg hover:bg-emerald-200 cursor-pointer">
-          Book 4
-        </li>
-      </ul>
+      {isLoading && <Loader />}
+      {error && <ErrorMessage message={error} />}
+      {!isLoading && !error && (
+        <>
+          <h2 className="text-xl font-semibold text-emerald-800 mb-4">
+            Search Results
+          </h2>
+          <ul className="space-y-2">
+            {books.map((book) => {
+              const imgLink = book.volumeInfo.imageLinks?.thumbnail
+              const title = book.volumeInfo.title
+              const author = book.volumeInfo.authors || ['Unknown author']
+              return (
+                <li
+                  className="p-3 bg-emerald-100 rounded-lg hover:bg-emerald-200 cursor-pointer"
+                  key={book.id}
+                >
+                  <div className="flex items-center space-x-4">
+                    <img
+                      src={imgLink}
+                      alt="Book Cover"
+                      className="w-12 h-18 object-cover rounded"
+                    />
+                    <div className="flex flex-col justify-center text-center flex-1">
+                      <h3 className="font-semibold text-emerald-800">
+                        {title}
+                      </h3>
+                      <p className="text-sm text-emerald-700">{author[0]}</p>
+                    </div>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </>
+      )}
     </section>
   )
+}
+
+LeftSection.propTypes = {
+  books: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+}
+
+function Loader() {
+  return <h1 className="text-emerald-700 font-semibold text-lg">LOADING...</h1>
+}
+
+function ErrorMessage({ message }) {
+  return (
+    <h1 className="text-red-700 font-semibold text-lg">
+      <span>🚫</span>
+      {message}
+    </h1>
+  )
+}
+
+ErrorMessage.propTypes = {
+  message: PropTypes.string.isRequired,
 }
 
 function RightSection() {
